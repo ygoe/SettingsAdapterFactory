a071d779b004730e9ef8b9bb25cf6f51d3524e6c
Yves Goergen <yves@unclassified.de>
2015-04-16 21:49:22 +0200

    Added Windows Forms state binding code, refactored WPF and Forms code into separate files

---
+++ SettingsDemo/Unclassified/Util/SettingsHelper.Forms.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) 2015, Yves Goergen, http://unclassified.software/source/settingsadapterfactory
+//
+// Copying and distribution of this file, with or without modification, are permitted provided the
+// copyright notice and this notice are preserved. This file is offered as-is, without any warranty.
+
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Unclassified.Util
+{
+	/// <summary>
+	/// Provides methods for specialized settings situations.
+	/// </summary>
+	public static partial class SettingsHelper
+	{
+		#region Window state handling
+
+		/// <summary>
+		/// Binds the window location, size and state to the settings. This method should be called
+		/// in the Form constructor after InitializeComponent.
+		/// </summary>
+		/// <param name="form">The window to update and monitor.</param>
+		/// <param name="settings">The settings to use for the window.</param>
+		public static void BindWindowState(Form form, IWindowStateSettings settings)
+		{
+			// Apply the current settings to the window, if available
+			if (settings.Left != int.MinValue &&
+				settings.Top != int.MinValue &&
+				settings.Width != int.MinValue &&
+				settings.Height != int.MinValue)
+			{
+				form.StartPosition = FormStartPosition.Manual;
+				form.Left = settings.Left;
+				form.Top = settings.Top;
+				form.Width = settings.Width;
+				form.Height = settings.Height;
+				form.WindowState = settings.IsMaximized ? FormWindowState.Maximized : FormWindowState.Normal;
+			}
+
+			// Write back any changes to the settings.
+			// The event signatures are different, so it's easier to just copy the handler code
+			// and let the compiler figure out the inferred types.
+			form.LocationChanged += (sender, args) =>
+			{
+				if (form.WindowState == FormWindowState.Normal)
+				{
+					settings.Left = (int) form.Bounds.Left;
+					settings.Top = (int) form.Bounds.Top;
+					settings.Width = (int) form.Bounds.Width;
+					settings.Height = (int) form.Bounds.Height;
+				}
+				settings.IsMaximized = form.WindowState == FormWindowState.Maximized;
+			};
+			form.SizeChanged += (sender, args) =>
+			{
+				if (form.WindowState == FormWindowState.Normal)
+				{
+					settings.Left = (int) form.Bounds.Left;
+					settings.Top = (int) form.Bounds.Top;
+					settings.Width = (int) form.Bounds.Width;
+					settings.Height = (int) form.Bounds.Height;
+				}
+				settings.IsMaximized = form.WindowState == FormWindowState.Maximized;
+			};
+		}
+
+		#endregion Window state handling
+
+		public static string ColorToString(Color color)
+		{
+			return "#" + (color.A != 255 ? color.A.ToString("x2") : "") +
+				color.R.ToString("x2") +
+				color.G.ToString("x2") +
+				color.B.ToString("x2");
+		}
+
+		public static Color StringToColor(string str)
+		{
+			if (str.StartsWith("#"))
+				str = str.Substring(1);
+			if (str.Length != 6 && str.Length != 8)
+				throw new FormatException("Invalid 6- or 8-digit hexadecimal color value.");
+			long value = long.Parse(str, System.Globalization.NumberStyles.AllowHexSpecifier);
+			if (str.Length == 6)
+				value |= 0xff000000;
+			return Color.FromArgb(
+				(byte) ((value >> 24) & 0xff),
+				(byte) ((value >> 16) & 0xff),
+				(byte) ((value >> 8) & 0xff),
+				(byte) (value & 0xff));
+		}
+	}
+}
---
+++ SettingsDemo/Unclassified/Util/SettingsHelper.Wpf.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2015, Yves Goergen, http://unclassified.software/source/settingsadapterfactory
+//
+// Copying and distribution of this file, with or without modification, are permitted provided the
+// copyright notice and this notice are preserved. This file is offered as-is, without any warranty.
+
+using System;
+using System.Linq;
+using System.Windows;
+
+namespace Unclassified.Util
+{
+	/// <summary>
+	/// Provides methods for specialized settings situations.
+	/// </summary>
+	public static partial class SettingsHelper
+	{
+		#region Window state handling
+
+		/// <summary>
+		/// Binds the window location, size and state to the settings. This method should be called
+		/// in the Window constructor after InitializeComponent.
+		/// </summary>
+		/// <param name="window">The window to update and monitor.</param>
+		/// <param name="settings">The settings to use for the window.</param>
+		public static void BindWindowState(Window window, IWindowStateSettings settings)
+		{
+			// Apply the current settings to the window, if available
+			if (settings.Left != int.MinValue &&
+				settings.Top != int.MinValue &&
+				settings.Width != int.MinValue &&
+				settings.Height != int.MinValue)
+			{
+				window.WindowStartupLocation = WindowStartupLocation.Manual;
+				window.Left = settings.Left;
+				window.Top = settings.Top;
+				window.Width = settings.Width;
+				window.Height = settings.Height;
+				window.WindowState = settings.IsMaximized ? WindowState.Maximized : WindowState.Normal;
+			}
+
+			// Write back any changes to the settings.
+			// The event signatures are different, so it's easier to just copy the handler code
+			// and let the compiler figure out the inferred types.
+			window.LocationChanged += (sender, args) =>
+			{
+				settings.Left = (int) window.RestoreBounds.Left;
+				settings.Top = (int) window.RestoreBounds.Top;
+				settings.Width = (int) window.RestoreBounds.Width;
+				settings.Height = (int) window.RestoreBounds.Height;
+				settings.IsMaximized = window.WindowState == WindowState.Maximized;
+			};
+			window.SizeChanged += (sender, args) =>
+			{
+				settings.Left = (int) window.RestoreBounds.Left;
+				settings.Top = (int) window.RestoreBounds.Top;
+				settings.Width = (int) window.RestoreBounds.Width;
+				settings.Height = (int) window.RestoreBounds.Height;
+				settings.IsMaximized = window.WindowState == WindowState.Maximized;
+			};
+		}
+
+		#endregion Window state handling
+	}
+}
